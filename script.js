6f94d1b85afe5a02ebd672b5fe0519189ce8c0ad
trena <20079502+tholst952@users.noreply.github.com>
2025-03-13 12:01:00 -0700

    set event listeners to hide menu on link click

--- script.js
+++ script.js
@@ -2,11 +2,26 @@
 
 const btn = document.querySelector(".btn");
 const img = document.querySelector("img");
+const navBtn = document.querySelector(".nav-btn");
+const navMenu = document.querySelector("nav");
+const navLinks = document.querySelectorAll(".nav-link");
 
+//Image hover effect
 btn.addEventListener("mouseenter", function () {
   img.src = "/img/avatar-12.png";
 });
 
 btn.addEventListener("mouseleave", function () {
   img.src = "/img/avatar-11.png";
+});
+
+navBtn.addEventListener("click", function () {
+  navMenu.classList.add("active");
+});
+
+//Close nav menu when a link is clicked
+navLinks.forEach((link) => {
+  link.addEventListener("click", function () {
+    navMenu.classList.remove("active");
+  });
 });
